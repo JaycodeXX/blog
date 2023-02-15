62d0800c56f254417dbdba3405bf4de1e8c677fc
Onyeke Sunday Victor <diamondholdingslimited@gmail.com>
2023-02-15 23:23:44 +0100

    Create basic layouts and Functionalities

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let mobileMenu = document.getElementById("mobileMenu");
+
+function openMobileMenu(){
+    mobileMenu.style.display = "flex";
+}
+
+function closeMobileMenu(){
+    mobileMenu.style.display = "none";
+}
